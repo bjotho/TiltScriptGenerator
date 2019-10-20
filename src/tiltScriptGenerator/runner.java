0d8f43a046b2c8fad22e95945d8ec2006b9af04c
Bjorn Inge Thoresen <bithor15@uia.no>
2019-10-20 18:50:24 +0200

    model query gives output as triple

--- src/tiltScriptGenerator/runner.java
+++ src/tiltScriptGenerator/runner.java
@@ -6,6 +6,7 @@ import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.util.FileManager;
@@ -18,14 +19,27 @@ public class runner {
 	public static void main(String[] args) {
 		OntModel model = readFile(OntModelSpec.OWL_DL_MEM);
 		String defaultNameSpace = "http://www.uia.no/jpn/tilt#";
+		
 		Property hasSCTID = model.getProperty(defaultNameSpace + "hasSCTID");
 		System.out.println("Created property: " + hasSCTID);
+		
+		String format = "%-30s";
+		
 		ResIterator iter = model.listSubjectsWithProperty(hasSCTID);
 		System.out.println("Iterating...");
+		
 		while (iter.hasNext()) {
 			Resource r = iter.nextResource();
 			if (r.hasProperty(hasSCTID, "297976006")) {
-				System.out.println(r);
+				String triple = String.format(format, r.getLocalName().toString());
+				Property hasValue = model.getProperty(defaultNameSpace + "hasValue");
+				triple += String.format(format, hasValue.getLocalName().toString());
+				RDFNode rValueNode = r.getProperty(hasValue).getObject();
+				Resource rValue = model.getResource(rValueNode.toString());
+				Property hasValueValue = model.getProperty(defaultNameSpace + "hasValueValue");
+				RDFNode value = rValue.getProperty(hasValueValue).getObject();
+				triple += String.format(format, value.toString());
+				System.out.println(triple);
 			}
 		}
 		System.out.println("Done");
